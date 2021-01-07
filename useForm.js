6a5cf51d91cb5b9a627f734024b293ff45456805
Joan Reig Roman <joan@MacBook-MacBook-Pro-de-Joan.local>
2021-01-07 20:09:44 +0100

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+
+export default function useForm( initialState = {}) {
+    
+
+    const [values, setValues] = useState(initialState);
+
+    const reset = ()=>{
+        setValues(initialState);
+    }
+
+    const handleInputChange = ({ target }) => {
+
+
+        setValues({
+            ...values,
+            [target.name]:target.value
+        });
+    }
+
+    return [values, handleInputChange, reset];
+}
